1cbd291ba90584febcb46f0fa24412efde13eabf
samjoshuadud <calebjoshuaarmojallas@gmail.com>
2026-06-16 17:43:23 +0800

    initial flake setup

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Punisher's NixOS config";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./nixos/configuration.nix
+        home-manager.nixosModules.homeManager
+        {
+          home-manager.users.punisher = import ./home-manager/home.nix;
+        }
+      ];
+    };
+  };
+}
