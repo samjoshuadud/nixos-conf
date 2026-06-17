4186c02252f6f8909249cbc1d47f5197d12fb6d2
samjoshuadud <calebjoshuaarmojallas@gmail.com>
2026-06-17 15:01:29 +0800

    feat: added zen-browser community maintained

--- flake.nix
+++ flake.nix
@@ -7,15 +7,22 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: {
+  outputs = { self, nixpkgs, home-manager, zen-browser, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
+        {
+        nixpkgs.config.allowUnfree = true;
+        }
         ./nixos/configuration.nix
         home-manager.nixosModules.home-manager  # ← fix here
         {
+          home-manager.useGlobalPkgs = true;
+          home-manager.extraSpecialArgs = { zenBrowser = zen-browser; };
           home-manager.users.punisher = import ./home-manager/home.nix;
         }
       ];
