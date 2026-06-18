fbe70f0fa22213e9dd3bcfdcfedb0c67f7af6404
samjoshuadud <calebjoshuaarmojallas@gmail.com>
2026-06-19 00:33:31 +0800

    fix: replace plain tmux pane bindings with pane_current_command detection for seamless vim/tmux navigation

--- dotfiles/nvim/.config/nvim/init.lua
+++ dotfiles/nvim/.config/nvim/init.lua
@@ -32,7 +32,6 @@ vim.keymap.set('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
-
 -- Remove ~ in nvim
 vim.opt.fillchars = { eob = ' ' } -- Replace empty lines with space
 
