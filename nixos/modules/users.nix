58e8cfb0a373fcd8559d2c0fca19c23aee4cdc3b
samjoshuadud <calebjoshuaarmojallas@gmail.com>
2026-06-30 01:23:02 +0800

    feat: added libvrd

--- nixos/modules/users.nix
+++ nixos/modules/users.nix
@@ -3,7 +3,7 @@
   users.users.punisher = {
     isNormalUser = true;
     description = "punisher";
-    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    extraGroups = [ "networkmanager" "wheel" "docker" "kvm" "libvirtd"];
     shell = pkgs.fish;
   };
 }
