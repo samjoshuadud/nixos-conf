855143b574f62e1ead7662c93ec747e8c926a751
samjoshuadud <calebjoshuaarmojallas@gmail.com>
2026-06-30 23:15:17 +0800

    feat: tuigreet parameters

--- nixos/modules/services.nix
+++ nixos/modules/services.nix
@@ -37,7 +37,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --time-format '%I:%M %p | %A, %B %d, %Y' --greeting 'Welcome to NixOS' --greet-align center --width 70 --window-padding 2 --container-padding 2 --prompt-padding 1 --asterisks --asterisks-char '•' --remember --remember-user-session --theme 'border=cyan;title=light_cyan;greet=light_blue;prompt=white;text=light_white;input=cyan;action=blue;button=light_cyan;time=light_blue' --cmd start-hyprland";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --time-format '%I:%M %p | %A, %B %d, %Y' --greeting 'Welcome to NixOS' --greet-align center --width 70 --window-padding 2 --container-padding 2 --prompt-padding 1 --asterisks --asterisks-char '•' --remember --remember-user-session --cmd start-hyprland";
         user = "greeter";
       };
     };
