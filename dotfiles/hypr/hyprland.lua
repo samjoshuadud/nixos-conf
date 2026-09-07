68426bca62f804420ec92a2a4c83cfce43c2ad1d
samjoshuadud <calebjoshuaarmojallas@gmail.com>
2026-09-07 16:21:25 +0800

    feat: second monitor support

--- dotfiles/hypr/hyprland.lua
+++ dotfiles/hypr/hyprland.lua
@@ -5,13 +5,36 @@ hl.monitor({
     scale    = 1,
 })
 
+-- For normal uses:
+-- hl.monitor({
+--     output   = "",
+--     mode     = "1920x1080",
+--     position = "auto",
+--     scale    = 1,
+--     mirror   = "eDP-1",
+-- })
+
+
+-- Second monitor RULES
 hl.monitor({
-    output   = "",
-    mode     = "1920x1080",
+    output   = "HDMI-A-1",
+    mode     = "1920x1080@100",
     position = "auto",
     scale    = 1,
-    mirror   = "eDP-1",
 })
+
+hl.workspace_rule({ workspace = "1", monitor = "eDP-1", default = true, persistent = true })
+hl.workspace_rule({ workspace = "2", monitor = "HDMI-A-1", default = true, persistent = true })
+hl.workspace_rule({ workspace = "3", monitor = "HDMI-A-1", default = true, persistent = true })
+hl.workspace_rule({ workspace = "4", monitor = "HDMI-A-1", default = true, persistent = true })
+hl.workspace_rule({ workspace = "5", monitor = "HDMI-A-1", default = true, persistent = true })
+hl.workspace_rule({ workspace = "6", monitor = "HDMI-A-1", default = true, persistent = true })
+hl.workspace_rule({ workspace = "7", monitor = "HDMI-A-1", default = true, persistent = true })
+hl.workspace_rule({ workspace = "8", monitor = "HDMI-A-1", default = true, persistent = true })
+hl.workspace_rule({ workspace = "9", monitor = "HDMI-A-1", default = true, persistent = true })
+hl.workspace_rule({ workspace = "10", monitor = "HDMI-A-1", default = true, persistent = true })
+
+
 
 require("source/environment")
 require("source/binds")
